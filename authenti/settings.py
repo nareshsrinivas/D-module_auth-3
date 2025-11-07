d3367503c9d6c410a28b712b1ee4798010993481
Naresh Srinivas Nagula <163895729+nareshsrinivas@users.noreply.github.com>
2025-11-07 18:32:09 +0530

    Update settings.py

--- authenti/settings.py
+++ authenti/settings.py
@@ -82,27 +82,12 @@ WSGI_APPLICATION = 'authenti.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'user_db',
-        'USER': 'postgres',
-        'PASSWORD': '8520',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
